9e261a2942bde535c0c1bfc543a1525beb302749
bao <mailforbao@gmail.com>
2022-11-22 12:58:22 +0700

    add prisma

---
+++ src/services/prisma.module.ts
@@ -0,0 +1,8 @@
+// import { PrismaService } from './prisma.service';
+// import { Global, Module } from '@nestjs/common';
+// @Global()
+// @Module({
+//   providers: [PrismaService],
+//   exports: [PrismaService],
+// })
+// export class PrismaModule {}
