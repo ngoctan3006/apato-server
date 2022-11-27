e660fa86d3252ed0a39e6f549215913196ae3ba0
bao <mailforbao@gmail.com>
2022-11-27 15:17:12 +0700

    test vercel

--- src/strategies/jwt.strategie.ts
+++ src/strategies/jwt.strategie.ts
@@ -13,7 +13,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: 'apato-token',
+      secretOrKey: config.get<string>('jwt_secret'),
     });
   }
 
