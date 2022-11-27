ba7153034df8fc5b8d60dfda0878ba82baeeaf6a
bao <mailforbao@gmail.com>
2022-11-27 17:17:39 +0700

    fix-config

--- src/strategies/jwt.strategie.ts
+++ src/strategies/jwt.strategie.ts
@@ -13,7 +13,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: config.get('JWT_SECRET'),
+      secretOrKey: new ConfigService().get('JWT_SECRET'),
     });
   }
 
