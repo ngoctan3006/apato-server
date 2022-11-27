30b228590e0483e7a46ee571b11e16ca3346dbdf
bao <mailforbao@gmail.com>
2022-11-27 15:55:05 +0700

    add cors

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,13 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CommonModule } from './services/common.module';
-
+import * as cors from 'cors';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = app.select(CommonModule).get(ConfigService);
   const port = config.get<number>('port');
   app.useGlobalPipes(new ValidationPipe());
+  app.use(cors());
   await app.listen(port);
 }
 bootstrap();
