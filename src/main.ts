34a5e9947ec9b5db76536b6789e1464fff3ba048
Hoang Ngoc Bao <79911907+letgobao@users.noreply.github.com>
2022-12-19 14:50:38 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CommonModule } from './services/common.module';
 import { join } from 'path';
+import * as cors from 'cors;
 import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const config = app.select(CommonModule).get(ConfigService);
   const port = config.get<number>('PORT');
   app.useGlobalPipes(new ValidationPipe());
-
+  app.use(cors({
+    origin: '*',
+  }))
   app.useStaticAssets(join(__dirname, '..', 'uploads'), {
     prefix: '/uploads/',
   });
